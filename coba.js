608c02ed811fc0d0127b54ca66e6918c1d138268
Dedesyarifudin07 <dedesyarifudinalhadid@gmail.com>
2022-09-10 23:38:12 +0700

    belajar membuat data json sendiri

--- coba.js
+++ coba.js
@@ -4,6 +4,7 @@
 const fs = require('fs');
 
 
+
 //menuliskan  string ke file secara (synchronus)
 // try{
 //     fs.writeFileSync('data/test.txt','hello deteok secara synchronus!');
@@ -12,6 +13,53 @@ const fs = require('fs');
 // }
 
 //menuliskan  string ke file secara (assynchronus)
-fs.writeFile('data/text.txt','Hello world secara asynchronus',(e) =>{
-    console.log(e)
-})
+// fs.writeFile('data/text.txt','Hello world secara asynchronus',(e) =>{
+//     console.log(e)
+// })
+
+//membaca isi file synchronus
+// const data = fs.readFileSync('data/test.txt','utf-8');
+// console.log(data);
+
+//membaca file asynchronus
+// fs .readFile('data/test.txt','utf-8',(err,data) => {
+//     if(err) throw err;
+//     console.log(data)
+// })
+
+
+//readline
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input:process.stdin,
+    output: process.stdout
+});
+
+rl.question('siapa nama anda', (nama) => {
+    rl.question('masukan nomer hp:', (hp) => {
+        //meyiapkan object
+        const  Contact = {nama,hp};
+       const file = fs.readFileSync('data/Contact.json','utf-8')
+       //ubah string ke json
+        const Contacts = JSON.parse(file);
+
+
+        Contacts.push(Contact);
+
+        console.log(Contact);
+
+        //ubah json ke string
+        fs.writeFileSync('data/Contact.json',JSON.stringify(Contacts));
+
+        console.log('terima kasih sudah memasukan data');
+
+        rl.close();
+    });
+  });
+
+
+//ketika menuliskan write file 
+//masukan ke contaxt.json
+
+
