51ca8cd66b9e9c2d8d24799d331220bf87d60845
Dedesyarifudin07 <dedesyarifudinalhadid@gmail.com>
2022-09-10 22:31:17 +0700

    menambahkan isi file

--- coba.js
+++ coba.js
@@ -1,1 +1,17 @@
-let = "dede"
+// core module
+//file system
+
+const fs = require('fs');
+
+
+//menuliskan  string ke file secara (synchronus)
+// try{
+//     fs.writeFileSync('data/test.txt','hello deteok secara synchronus!');
+// }catch(err){
+//     console.log(err)
+// }
+
+//menuliskan  string ke file secara (assynchronus)
+fs.writeFile('data/text.txt','Hello world secara asynchronus',(e) =>{
+    console.log(e)
+})
