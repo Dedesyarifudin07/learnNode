71d02d252ef1c4979f90060b626389e6901b890f
Dedesyarifudin07 <dedesyarifudinalhadid@gmail.com>
2022-09-15 15:55:30 +0700

    menambahkan packgae

--- app.js
+++ app.js
@@ -1,2 +1,37 @@
-console.log('hello world');
+const fs = require('fs');
+
+//readline
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input:process.stdin,
+    output: process.stdout
+});
+
+rl.question('siapa nama anda', (nama) => {
+    rl.question('masukan nomer hp:', (hp) => {
+        //meyiapkan object
+        const  Contact = {nama,hp};
+        //baca filesystem
+       const file = fs.readFileSync('data/Contact.json','utf-8')
+       //ubah string ke json
+        const Contacts = JSON.parse(file);
+
+
+        Contacts.push(Contact);
+
+        console.log(Contact);
+
+        //ubah json ke string
+        fs.writeFileSync('data/Contact.json',JSON.stringify(Contacts));
+
+        console.log('terima kasih sudah memasukan data');
+
+        rl.close();
+    });
+  });
+
+
+//ketika menuliskan write file 
+//masukan ke contaxt.json
 
